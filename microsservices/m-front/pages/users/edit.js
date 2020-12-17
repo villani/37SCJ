388e67cb708a37c5c8e5b65d3b09f68a5fc2814b
Leonardo Villani <leonardo.villani@fatec.sp.gov.br>
2020-12-17 13:37:30 -0300

    Inclusão de formulário para edição de usuário.

---
+++ microsservices/m-front/pages/users/edit.js
@@ -0,0 +1,60 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+export default function Edit() {
+  return (
+    <Container>
+        <Row>
+            <Col>
+              <h1>Edição de Usuário</h1>
+              <Form>
+                <Form.Group controlId="name">
+                  <Form.Label>Nome:</Form.Label>
+                  <Form.Control type="text" placeholder="Informe o nome do usuário." />
+                </Form.Group>
+
+                <Form.Group controlId="email">
+                  <Form.Label>E-mail</Form.Label>
+                  <Form.Control type="email" placeholder="Informe o e-mail." />
+                </Form.Group>
+
+                <Form.Group controlId="password">
+                  <Form.Label>Senha:</Form.Label>
+                  <Form.Control type="password" placeholder="Senha" />
+                </Form.Group>
+
+                <Form.Group controlId="maritalStatus">
+                  <Form.Label>Estado civil:</Form.Label>
+                  <Form.Control as="select" defaultValue="Solteiro(a)">
+                    <option>Solteiro(a)</option>
+                    <option>Casado(a)</option>
+                    <option>Divorciado(a)</option>
+                    <option>União estável</option>
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId="birthDate">
+                  <Form.Label>Data de nascimento:</Form.Label>
+                  <Form.Control type="date" />
+                </Form.Group>
+
+                <Form.Group controlId="allowNotifications">
+                  <Form.Check
+                    required
+                    label="Permitir notificações"
+                    feedback="Você precisa permitir se desejar ser notificado."
+                  />
+                </Form.Group>
+               
+                <Button variant="primary" type="submit">
+                  Gravar
+                </Button>
+              </Form>
+            </Col>
+        </Row>
+    </Container>
+  )
+}
